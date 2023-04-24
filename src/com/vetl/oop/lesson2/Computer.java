bc7d31c58094b92dc870eb683f3d5e96c2e756fa
VetlJava <vetl.foto@gmail.com>
2023-04-24 15:46:41 +0300

    lesson 3.1 Конструкторы

--- src/com/vetl/oop/lesson2/Computer.java
+++ src/com/vetl/oop/lesson2/Computer.java
@@ -2,7 +2,20 @@ package com.vetl.oop.lesson2;
 
 public class Computer {
     int ssd = 500;
-    int ram = 1024;
+    int ram;
+
+    Computer() {
+        System.out.println("Я был создан");
+    }
+
+    Computer(int newSsd) {
+        ssd = newSsd;
+    }
+
+    Computer(int newSsd, int newRam) {
+        ssd = newSsd;
+        ram = newRam;
+    }
 
     void load() {
         System.out.println("Я загрузился");
@@ -11,5 +24,6 @@ public class Computer {
     void printState() {
         System.out.println("Ssd: " + ssd);
         System.out.println("Ram: " + ram);
+        System.out.println();
     }
 }
